07c0c223a61708fe1c9da22b04f5b1e57a22a334
mkinski <none>
2012-05-26 21:55:20 +0200

    change version to 0.2

--- init.rb
+++ init.rb
@@ -4,7 +4,7 @@ Redmine::Plugin.register :redmine_inventory_manager do
   name 'Redmine Inventory Manager Plugin'
   author 'Daniel Anguita O.'
   description 'Take your warehouse or office inventory on the same platform of your projects'
-  version '0.0.1'
+  version '0.2'
   
   requires_redmine :version_or_higher => '2.0.0'
   
